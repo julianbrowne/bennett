8b14ed23d6cff2577991ac2145432c01b0c459b6
Julian Browne <julianbrowne@gmail.com>
2013-08-28 12:00:49 +0100

    jasmine tests II

--- test/spec/piggybankSpec.js
+++ test/spec/piggybankSpec.js
@@ -76,10 +76,142 @@ describe("Piggybank", function() {
 
   });
 
-  describe("Advanced operations", function() { 
+
+  describe("Basic Responses", function() { 
+
+    var request;
+    var onSuccess;
+    var onFailure;
+    var http200 = { status: 200, responseText: { "message": "OK" } };
+    var http400 = { status: 400, responseText: { "message": "Bad Request" } };
+    var http404 = { status: 404, responseText: { "message": "Not Found" } };
+    var publish = { show: function(result) { console.log("*****"); console.log(result); } };
 
     beforeEach(function() { 
+
       jasmine.Ajax.useMock();
+
+      //onSuccess = jasmine.createSpy('onSuccess');
+      //onFailure = jasmine.createSpy('onFailure');
+      //piggy.ajaxSuccess = onSuccess;
+      //piggy.ajaxFailure = onFailure;
+
+      spyOn(piggy, 'ajaxCall').andCallThrough();
+      spyOn(publish, 'show');
+
+      piggy.addCall("/somewhere", { method: "get" });
+      piggy.makeCallsSynchronously().done(publish.show);
+
+      request = mostRecentAjaxRequest();
+      expect(request.url).toBe('http://www.example.com/api/somewhere');
+      expect(request.method).toBe('GET');
+
+    });
+
+    describe("return codes", function() { 
+
+      beforeEach(function() { 
+      });
+
+      it("get a 200 OK response", function() { 
+
+        request.response(http200);
+
+        expect(request.status).toEqual(200);
+        expect(request.responseText).toEqual({ "message": "OK" });
+        expect(piggy.ajaxCall).toHaveBeenCalled();
+
+      });
+
+      it("get a 404 Not Found response", function() { 
+
+        request.response(http404);
+
+        expect(request.status).toEqual(404);
+        expect(request.responseText).toEqual({ "message": "Not Found" });
+        expect(piggy.ajaxCall).toHaveBeenCalled();
+
+      });
+
+    });
+
+  });
+
+  describe("Fake Server", function() { 
+
+    var http400 = { status: 400, responseText: { "message": "Bad Request" } };
+    var resultData;
+    var config = { 
+      url: '/users',
+      status: 200,
+      responseTime: 300,
+      method: 'get',
+      response: '{ "firstName": "fred", "lastName":  "Smith", "age": 99 }',
+      encoding: "form",
+      name: "jasmine fake server get /users/42",
+      remember: "memory",
+      schema: { 
+        "type":"object",
+        "properties": { 
+          "firstName": { "type":"string" },
+          "lastName": {  "type":"string" },
+          "age": { "type": "integer" }
+        },
+        "required": [ 
+          "firstName",
+          "lastName",
+          "age"
+        ]
+      },
+      body: { id: "42" }
+    };
+
+    beforeEach(function() { 
+
+      $.mockjax({ 
+          url: "http://www.example.com/api" + config.url,
+          responseText: config.response,
+          responseTime: config.responseTime,
+          status: config.status
+      });
+
+      piggy.addCall(config.url, { 
+        method: config.method, 
+        name: config.name,
+        body: config.body,
+        schema: config.schema,
+        remember: config.remember,
+        encoding: config.encoding
+      });
+      piggy.makeCallsSynchronously().done(function(data) { resultData = data; });
+      waitsFor(
+        function() { 
+          return resultData !== undefined;
+        },
+        "call timed out",
+        10000
+      );
+    });
+
+    it("should activate callback with data", function() { 
+      expect(resultData).toBeDefined();
+    });
+
+    it("should have 200 response in callback", function() { 
+      expect(resultData[0].status).toEqual(config.status);
+    });
+
+    it("should see original name in callback", function() { 
+      expect(resultData[0].data.name).toEqual(config.name);
+    });
+
+    it("should build correctly encoded body", function() { 
+      expect(resultData[0].data.encoding).toEqual(config.encoding);
+      expect(resultData[0].data.body).toEqual("id=" + config.body.id);
+    });
+
+    it("should validate correct json against schema", function() { 
+      expect(resultData[0].data.schemaCheck.valid).toEqual(true);
     });
 
   });
